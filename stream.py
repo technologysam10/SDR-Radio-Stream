a6f34a1eed0a30b50d717c40a799c564c3af2f32
Sam <samrichardsongph@gmail.com>
2025-06-18 19:42:06 -0400

    add basic flask skeletion and first test of tuning using rtlfm

---
+++ stream.py
@@ -0,0 +1,7 @@
+import subprocess
+global cmd
+
+def openFM(frequency):
+    cmd = ["rtl_fm", "-f", f"{frequency}M", "-M", "WBFM"]
+    demodulated = subprocess.run(cmd)
+    print(demodulated.stdout)
