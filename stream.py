53ce5731fe2b0ce60b493890697f9a69a8c3ddbc
Sam <samrichardsongph@gmail.com>
2025-06-18 20:55:07 -0400

    redo base url and add input sample rate

--- stream.py
+++ stream.py
@@ -1,9 +1,9 @@
 import subprocess
 global cmd
-baseurl = "http://192.168.86.26:8234"
+baseurl = "http://0.0.0.0:8234"
 def openFM(frequency):
     samplerate = 48000
-    rtlfm_cmd = ["rtl_fm", "-f", f"{frequency}M", "-M", "WBFM", "-r", str(samplerate)]
+    rtlfm_cmd = ["rtl_fm", "-f", f"{frequency}M", "-s", "200000", "-M", "WBFM", "-r", str(samplerate)]
     rtlfm_process = subprocess.Popen(rtlfm_cmd, stdout=subprocess.PIPE)
     sox_cmd = ["sox", "-t", "raw", "-r", str(samplerate), "-e", "signed", "-b", "16", "-c", "1", "-", "-t", "wav", "-"]
     sox_process = subprocess.Popen(sox_cmd, stdin=rtlfm_process.stdout, stdout=subprocess.PIPE)
