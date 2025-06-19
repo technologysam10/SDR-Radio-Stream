361d4143ab3118410bcbfac9af05c85843e2962a
Sam <samrichardsongph@gmail.com>
2025-06-18 20:34:43 -0400

    add in a string of pipes

--- stream.py
+++ stream.py
@@ -1,8 +1,16 @@
 import subprocess
 global cmd
-
+baseurl = ""
 def openFM(frequency):
-    cmd = ["rtl_fm", "-f", f"{frequency}M", "-M", "WBFM"]
-    demodulated = subprocess.run(cmd)
-    print(demodulated.stdout)
+    samplerate = 48000
+    rtlfm_cmd = ["rtl_fm", "-f", f"{frequency}M", "-M", "WBFM", "-r", str(samplerate)]
+    rtlfm_process = subprocess.Popen(rtlfm_cmd, stdout=subprocess.PIPE)
+    sox_cmd = ["sox", "-t", "raw", "-r", str(samplerate), "-e", "signed", "-b", "16", "-c", "1", "-", "-t", "wav", "-"]
+    sox_process = subprocess.Popen(sox_cmd, stdin=rtlfm_process.stdout, stdout=subprocess.PIPE)
+    ffmpeg_cmd = ["ffmpeg", "-f", "wav", "-i", "-", "-f", "mp3", f"{baseurl}/stream/{frequency}.mp3" ]
+
+        
+        
+
+
 openFM("93.7")
