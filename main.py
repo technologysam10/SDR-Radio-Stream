d0d3f8554133b945c16297e51f0f534354f9c183
Sam <samrichardsongph@gmail.com>
2025-06-19 15:56:14 -0400

    Read stdout and return 4kb of mp3

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route("/stream/<type>/<frequency>")
 def streamRoute(type, frequency):
     if type == "fm":
-        print("Opening FM stream:" + frequency)
-        stream.openFM(frequency)
-
+        print("Opening FM stream: " + frequency)
+        feed = stream.openFM(frequency)
+        while True:
+            chunk = feed.read(4096)
+            return chunk
 app.run(host="0.0.0.0", port=8234)
