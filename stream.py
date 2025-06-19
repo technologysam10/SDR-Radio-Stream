d1ae6a422bccbd9c1b49dec194de30c0a945d51a
Sam <samrichardsongph@gmail.com>
2025-06-18 20:40:33 -0400

    add forgotten pipe and hardcode base url for now.

--- stream.py
+++ stream.py
@@ -1,6 +1,6 @@
 import subprocess
 global cmd
-baseurl = ""
+baseurl = "http://192.168.86.26:8234"
 def openFM(frequency):
     samplerate = 48000
     rtlfm_cmd = ["rtl_fm", "-f", f"{frequency}M", "-M", "WBFM", "-r", str(samplerate)]
@@ -8,6 +8,7 @@ def openFM(frequency):
     sox_cmd = ["sox", "-t", "raw", "-r", str(samplerate), "-e", "signed", "-b", "16", "-c", "1", "-", "-t", "wav", "-"]
     sox_process = subprocess.Popen(sox_cmd, stdin=rtlfm_process.stdout, stdout=subprocess.PIPE)
     ffmpeg_cmd = ["ffmpeg", "-f", "wav", "-i", "-", "-f", "mp3", f"{baseurl}/stream/{frequency}.mp3" ]
+    ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdin=sox_process.stdout)
 
         
         
